a1118070ecfd45fb2d18b2ef9f65da96b5914787
testoerik <erikhag04@gmail.com>
2026-06-12 17:53:59 +0200

    .

--- src/JulianDay.java
+++ src/JulianDay.java
@@ -1,5 +1,5 @@
 public class JulianDay {
-    void calcJulianDay(int day, int month, int year) {
+    int JulianDay(int day, int month, int year) {
         if (month <= 2){
             month += 12;
             year -= 1;
@@ -7,7 +7,9 @@ public class JulianDay {
     int a = year / 100;
     int b = a / 4;
     int c = 2 - a + b;
-
+    int e = (int) (365.25 * (year + 4716));
+    int f = (int) (30.6001 * (month + 1));
+    return c + day + e + f - 1524;
     }
 }
 

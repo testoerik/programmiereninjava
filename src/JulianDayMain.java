477bf8a4c99f767d3760a50b3c19940cf95988b2
testoerik <erikhag04@gmail.com>
2026-06-12 18:19:45 +0200

    .

--- src/JulianDayMain.java
+++ src/JulianDayMain.java
@@ -1,7 +1,15 @@
 public class JulianDayMain {
     public static void main(String[] args) {
         if (args.length != 3) {
-            print*("Usage: ")
+            System.out.println("Usage: Day Month Year");
+            return;
         }
+        int day = Integer.parseInt(args[0]);
+        int month = Integer.parseInt(args[1]);
+        int year = Integer.parseInt(args[2]);
+
+        JulianDay result = new JulianDay();
+        result.calcJulianDay(day, month, year);
+        printf("Julian day for %02d.%02d.%04d: \n", day, month, year, result);
     }
 }
